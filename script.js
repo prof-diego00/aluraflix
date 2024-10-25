448b0826fa0ceb9d5be244d60c563c2f90d2c5aa
Prof. Diego <diegosa@professor.educacao.sp.gov.br>
2024-10-25 11:06:58 -0300

    Projeto Final

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js
+console.log("Hello, world!");
+
+function minhaFuncao() {
+    alert("Esta função foi chamada do JavaScript!");
+}
+const scrollContainer = document.querySelector('.categoria-videos');
+const scrollLeftButton = document.querySelector('.scroll-buttons .left-button');
+const scrollRightButton = document.querySelector('.scroll-buttons .right-button');
+
+scrollLeftButton.addEventListener('click', () => {
+    scrollContainer.scrollLeft -= 200; // Ajuste a quantidade de rolagem conforme necessário
+});
+
+scrollRightButton.addEventListener('click', () => {
+    scrollContainer.scrollLeft += 200; // Ajuste a quantidade de rolagem conforme necessário
+});
+
+$(document).ready(function() {
+    // Ocultar a barra de rolagem ao carregar a página
+    $('body').css('overflow-y', 'hidden');
+
+    // Mostrar a barra de rolagem ao clicar em um botão
+    $('#mostrar-scrollbar').click(function() {
+        $('body').css('overflow-y', 'auto');
+    });
+});
